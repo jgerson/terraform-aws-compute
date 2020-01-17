007d75f93845790ecdc862246a3145518b3ce1de
Jeremy Gerson <jgerson@hashicorp.com>
2020-01-17 13:08:29 -0500

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -13,7 +13,7 @@ data "terraform_remote_state" "vpc" {
 module "security-group" {
   source  = "app.terraform.io/jgersonorg1/security-group/aws"
   version = "3.1.0"
-  name    = "${var.prefix}-sg-demo-jg"
+  name    = "${var.prefix}-sg-demo"
   vpc_id  = data.terraform_remote_state.vpc.outputs.vpc_id 
   ingress_cidr_blocks      = ["10.10.0.0/16"]
   ingress_rules           = ["https-443-tcp"]
